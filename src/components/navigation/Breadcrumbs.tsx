91cd9ff7570fd19416c328464100ea334f4b11b3
Magdalena Magličić <magdalena.maglicic@fiveminutes.eu>
2021-01-18 12:58:42 +0100

    Breadcrumb links added

--- src/components/navigation/Breadcrumbs.tsx
+++ src/components/navigation/Breadcrumbs.tsx
@@ -14,12 +14,12 @@ export function Breadcrumbs() {
   
   return (
     <div className="flex justify-center center-content mt-32 space-x-16">
-      {breadcrumbs.map(({ breadcrumb }, index) => {
+      {breadcrumbs.map(({ breadcrumb, match: { url } }, index) => {
         const isLast = index === length - 1;
 
         return (
           <div key={`${index}-bread`} className={`flex space-x-16 items-center text-sm font-bold ${!isLast && 'text-primary'}`}>
-            <Link to="#">
+            <Link to={url}>
               {breadcrumb}
             </Link>
             {!isLast && <Right />}
